f60da7a6b2efb8852e3586fd943097168748da92
Ikrom67 <123659503+Ikrom67@users.noreply.github.com>
2026-03-16 10:23:18 +0000

    Update Vercel rewrites and SPA static fallback route

--- index.js
+++ index.js
@@ -65,7 +65,8 @@ const authenticate = async (req, res, next) => {
   }
 };
 
-app.get('/', (req, res) => {
+app.use(express.static(__dirname));
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
